6989951357063e3e2bacf020d07e29532ad8abd1
Armando Ruocco <armando.ruocco@enterprisedb.com>
2024-09-10 17:31:27 +0200

    fix: ensure status can always be set
    Signed-off-by: Armando Ruocco <armando.ruocco@enterprisedb.com>

--- internal/operator/status.go
+++ internal/operator/status.go
@@ -4,16 +4,14 @@ import (
 	"context"
 	"encoding/json"
 	"errors"
-	"fmt"
-
 	apiv1 "github.com/cloudnative-pg/cloudnative-pg/api/v1"
-	"github.com/cloudnative-pg/cnpg-i-hello-world/pkg/metadata"
 	"github.com/cloudnative-pg/cnpg-i-machinery/pkg/logging"
-	"github.com/cloudnative-pg/cnpg-i-machinery/pkg/pluginhelper/common"
-
 	"github.com/cloudnative-pg/cnpg-i-machinery/pkg/pluginhelper/clusterstatus"
+	"github.com/cloudnative-pg/cnpg-i-machinery/pkg/pluginhelper/common"
 	"github.com/cloudnative-pg/cnpg-i-machinery/pkg/pluginhelper/decoder"
 	"github.com/cloudnative-pg/cnpg-i/pkg/operator"
+
+	"github.com/cloudnative-pg/cnpg-i-hello-world/pkg/metadata"
 )
 
 type Status struct {
@@ -26,7 +24,7 @@ func (Implementation) SetStatusInCluster(
 ) (*operator.SetStatusInClusterResponse, error) {
 	logger := logging.FromContext(ctx).WithName("cnpg_i_example_lifecyle")
 
-	cluster, err := decoder.DecodeClusterJSON(req.Cluster)
+	cluster, err := decoder.DecodeClusterJSON(req.GetCluster())
 	if err != nil {
 		return nil, err
 	}
@@ -43,22 +41,24 @@ func (Implementation) SetStatusInCluster(
 	if pluginEntry == nil {
 		err := errors.New("plugin entry not found in the cluster status")
 		logger.Error(err, "while fetching the plugin status", "plugin", metadata.PluginName)
-		return nil, fmt.Errorf("plugin entry not found")
+		return nil, errors.New("plugin entry not found")
 	}
 
 	var status Status
-	if err := json.Unmarshal([]byte(pluginEntry.Status), &status); err != nil {
-		logger.Error(err, "while unmarshalling plugin status")
-		return nil, err
+	if pluginEntry.Status != "" {
+		if err := json.Unmarshal([]byte(pluginEntry.Status), &status); err != nil {
+			logger.Error(err, "while unmarshalling plugin status",
+				"entry", pluginEntry)
+			return nil, err
+		}
 	}
-
 	if status.Enabled {
 		logger.V(0).Info("plugin is enabled, no action taken")
-		return clusterstatus.NewSetClusterStatusResponseBuilder().NoOpResponse(), nil
+		return clusterstatus.NewSetStatusInClusterResponseBuilder().NoOpResponse(), nil
 	}
 
 	// if for any reson the status want to be wiped out we can use the following:
 	// clusterstatus.NewSetClusterStatusResponseBuilder().SetEmptyStatusResponse()
 	logger.V(1).Info("setting enabled plugin status")
-	return clusterstatus.NewSetClusterStatusResponseBuilder().JSONStatusResponse(Status{Enabled: true})
+	return clusterstatus.NewSetStatusInClusterResponseBuilder().JSONStatusResponse(Status{Enabled: true})
 }
